7130e6453ca0cd25bc2a5a35ef0f24ea8f1ece0d
Bruce <bruce@nantekiMacBook-Pro.local>
2015-03-15 14:12:29 +0800

    commit to my iMac

--- pinpintong/MainViewController.h
+++ pinpintong/MainViewController.h
@@ -1,15 +1,13 @@
 //
-//  FirstViewController.h
+//  MainViewController.h
 //  pinpintong
 //
-//  Created by Bruce He on 15-3-14.
+//  Created by Bruce on 15-3-15.
 //  Copyright (c) 2015年 shanghai baiwei network technology. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface MixViewController : UINavigationController
-
+@interface MainViewController : UINavigationController
 
 @end
-
--- pinpintong/Mix/MixTableViewController.h
+++ pinpintong/Mix/MixTableViewController.h
@@ -1,15 +1,13 @@
 //
-//  FirstViewController.h
+//  MixTableViewController.h
 //  pinpintong
 //
-//  Created by Bruce He on 15-3-14.
+//  Created by Bruce on 15-3-15.
 //  Copyright (c) 2015年 shanghai baiwei network technology. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface MixViewController : UINavigationController
-
+@interface MixTableViewController : UITableViewController
 
 @end
-
