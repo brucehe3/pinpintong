5784b01a1f35701192b397c266293def16b146da
Bruce He <Bruce@BrucedeiMac.local>
2015-04-05 21:00:43 +0800

    login view

---
+++ pinpintong/LoginViewController.h
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.h
+//  pinpintong
+//
+//  Created by Bruce He on 15-4-5.
+//  Copyright (c) 2015年 shanghai baiwei network technology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+<UITextFieldDelegate>
+
+
+@end
