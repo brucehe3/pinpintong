f530fe70baa99596ec5628db9143edc67d91ef09
Bruce He <Bruce@BrucedeiMac.local>
2015-04-11 14:37:14 +0800

    new mixui

---
+++ pinpintong/MixUITableView.m
@@ -0,0 +1,9 @@
+//
+//  MixUITableView.m
+//  pinpintong
+//
+//  Created by Bruce He on 15-4-8.
+//  Copyright (c) 2015年 shanghai baiwei network technology. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
